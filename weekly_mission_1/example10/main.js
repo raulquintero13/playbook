4bcb90fc56c831d85320f0367954a7f46ded322b
Raul Quintero <raulquintero13@gmail.com>
2022-04-16 11:36:28 -0700

    adding exercise 10

---
+++ weekly_mission_1/example10/main.js
@@ -0,0 +1,11 @@
+/*
+  export default nos permite exportar esta clase e importara
+
+  import MyPokemon from './pokemon.js'
+*/
+import  MyPokemon from './pokemon.js'
+
+// console.log(MyPokemon)
+
+const pikachu = new MyPokemon('Pikachu')
+pikachu.sayHello()
