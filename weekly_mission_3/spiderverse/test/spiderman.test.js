fd9cb14dc25c1652104a39c98de7be313bd9d389
Raul Quintero <raulquintero13@gmail.com>
2022-04-24 13:39:17 -0700

    adding getInfo method & unit test for Spiderverse

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,7 +1,17 @@
 
 describe("test suites dummy description", ()=> {
     test('case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(13);
+        const Spiderman = require('../app/Spiderman')
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew garfield", 2, "Sony")
+
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
+
+        const tomHolland = new Spiderman("Spiderman Sony", 31, "Tom Holland", 2, "Marvel")
+
+        expect (tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
     })
 })
