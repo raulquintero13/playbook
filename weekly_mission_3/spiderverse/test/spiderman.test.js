e31563e982fa49b8b74b863b3ecf190bab7ed874
Raul Quintero <raulquintero13@gmail.com>
2022-04-24 13:18:21 -0700

    adding unit test in spiderman.test.js:=

---
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -0,0 +1,7 @@
+
+describe("test suites dummy description", ()=> {
+    test('case 1 Dummy', () => {
+        const resultOfSomething = 1 + 2
+        expect(resultOfSomething).toBe(13);
+    })
+})
