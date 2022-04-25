f1a8a52c59568ebbc2715834e7ac510275b0b0d9
Raul Quintero <raulquintero13@gmail.com>
2022-04-25 14:04:58 -0700

    add setters unit tests

--- weekly_mission_3/twitter/test/User.test.js
+++ weekly_mission_3/twitter/test/User.test.js
@@ -19,4 +19,14 @@ describe("Unit tests  for user object", ()=>{
         expect(user.getDateCreated).not.toBeUndefined()
         expect(user.getLastUpdated).not.toBeUndefined()
     })
+
+    test('Add setters', ()=> {
+        const user = new User(1, "carlogilmar", " Carlo", "Bio")
+        user.setUsername = "Gilmar"
+        expect(user.username).toBe("Gilmar")
+        
+        user.setBio = "New Bio"
+        expect(user.bio).toBe("New Bio")
+
+    })
 })
