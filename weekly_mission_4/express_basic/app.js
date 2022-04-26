cebdaf5a8fd02bbe418f524ddaff4cc0e6478470
Raul Quintero <raulquintero13@gmail.com>
2022-04-26 16:59:45 -0700

    express - nueva ruta

--- weekly_mission_4/express_basic/app.js
+++ weekly_mission_4/express_basic/app.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
     res.send('hello world!')
 })
 
+
+app.get('/launchx', ( req, res) => {
+    res.send('Bienvenidos a launchx')
+})
+
+
 app.listen(port,() => {
     console.log(`example app listening on port ${port}`)
 })
