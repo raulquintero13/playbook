a84a6b7c30d8b03d31834a216641d227b3cfc240
Raul Quintero <raulquintero13@gmail.com>
2022-04-26 17:05:25 -0700

    express - QueryParams

--- weekly_mission_4/express_basic/app.js
+++ weekly_mission_4/express_basic/app.js
@@ -17,6 +17,10 @@ app.get('/explorersInNode', (req, res) => {
     res.send(explorer)
 })
 
+app.get('/explorers/:explorerName',(req, res) => {
+    res.send(req.params)
+})
+
 app.listen(port,() => {
     console.log(`example app listening on port ${port}`)
 })
