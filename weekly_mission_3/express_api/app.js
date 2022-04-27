08b6b2fd2cd346d9e3dd2f1587d8c8373de654f3
Raul Quintero <raulquintero13@gmail.com>
2022-04-26 19:19:06 -0700

    express api - update & delete endpoints

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -32,6 +32,18 @@ app.post('/v1/explorers', (req,res) => {
     res.status(201).json({message: "created"})
 })
 
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body
+    res.status(200).json({message: "updated!"})
+})
 
 
 
+app.delete('/v1/explorers/:id', (req, res)=> {
+    console.log(`Api Explorers DELETE reque ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body
+    res.status(200).json({message: "Deleted"})
+})
