cc4dde895536c096c183948b083ee0c4868dd59c
Raul Quintero <raulquintero13@gmail.com>
2022-04-26 17:02:53 -0700

    express - regresa un objeto

--- weekly_mission_4/express_basic/app.js
+++ weekly_mission_4/express_basic/app.js
@@ -12,6 +12,10 @@ app.get('/launchx', ( req, res) => {
     res.send('Bienvenidos a launchx')
 })
 
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {anme: "explorer", msg: "hello"}
+    res.send(explorer)
+})
 
 app.listen(port,() => {
     console.log(`example app listening on port ${port}`)
